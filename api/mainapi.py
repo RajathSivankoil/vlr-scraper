1e5e4ff0b0f44b7d9724e9b5fae96cd1d1a57d0c
Rajath Sivankoil <rajathsivankoil@icloud.com>
2025-04-14 06:31:10 -0400

    added an icon because i thought it was funny

--- api/mainapi.py
+++ api/mainapi.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from get_all_player_stats import get_all_player_stats
 from get_player_agent_stats import get_player_agent_stats
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("favicon.ico")
 
 @app.get("/player/{player_id}")
 def get_player_agent_stats_endpoint(player_id: int, timespan: int = 30):
@@ -13,8 +19,8 @@ def get_player_agent_stats_endpoint(player_id: int, timespan: int = 30):
     return JSONResponse(content=player_data)
 
 @app.get("/players/{region}")
-def get_all_player_stats_endpoint(region: str, timespan: int = 30):
-    player_data = get_all_player_stats.get_all_player_stats(region, timespan)
+def get_all_player_stats_endpoint(region: str = "all", timespan: int = 60):
+    player_data = get_all_player_stats(region, timespan)
     if player_data is None:
         return JSONResponse(status_code=404, content={"message": "No data found"})
     return JSONResponse(content=player_data)
